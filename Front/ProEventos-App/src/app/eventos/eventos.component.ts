5f64fea8fc573501c83594745fbd8b9b5ed6555e
Bruno Sajermann <contato@sajermann.com>
2021-04-12 12:49:43 -0300

    Seção 20 V2 Angular Interpolação, Diretivas e Bindings Video 214 Enviando ao Git

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventsFiltred: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImage: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(): string{
+    return this._filterList;
+  }
+
+  public set filterList(value: string){
+    this._filterList = value;
+    this.eventsFiltred = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+  }
+
+  filterEvents(filterFor: string): any{
+    filterFor = filterFor.toLocaleLowerCase();
+    return this.eventos.filter(evento=> evento.tema.toLocaleLowerCase().indexOf(filterFor) !== -1 ||
+    evento.local.toLocaleLowerCase().indexOf(filterFor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -18,7 +39,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos():void{
     this.http.get('http://localhost:5000/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventsFiltred = response
+      },
       error => console.log(error)
     );
   }
